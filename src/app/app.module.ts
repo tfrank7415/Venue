e43233247a25dd910d70c0dd936de8e9818129dc
johntylerfranklin <johntylerfranklin@gmail.com>
2020-01-25 19:40:14 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+     AppRoutingModule,
+     BrowserAnimationsModule,
+     MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
